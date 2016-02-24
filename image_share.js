d37886507bd78cb971544ffcfb1c0f3b2b54d627
Raman <balyan.raman@gmail.com>
2016-02-24 12:18:53 +0530

    template helper added

--- image_share.js
+++ image_share.js
@@ -1,23 +1,12 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  var img_data = {
+    img_src:"hut.jpg",
+    img_alt:"hut on the beach"
+  };
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  Template.images.helpers(img_data);
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  console.log("I am server");
 }
