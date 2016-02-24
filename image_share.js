2bdc4dfbfcf15d82219122906b19569222a03144
Raman <balyan.raman@gmail.com>
2016-02-24 12:28:00 +0530

    array added

--- image_share.js
+++ image_share.js
@@ -1,10 +1,21 @@
 if (Meteor.isClient) {
-  var img_data = {
+  var img_data = [
+  {
     img_src:"hut.jpg",
     img_alt:"hut on the beach"
-  };
+  },
+  {
+    img_src:"bike.jpg",
+    img_alt:"bike in desert"
+  },
+  {
+    img_src:"bike2.jpg",
+    img_alt:"bike2 in desert"
+  },
 
-  Template.images.helpers(img_data);
+  ];
+
+  Template.images.helpers({images:img_data});
 }
 
 if (Meteor.isServer) {
