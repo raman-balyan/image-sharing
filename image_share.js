35a804ad399d41baec5f493f442e0d0b27ac63a2
Raman <balyan.raman@gmail.com>
2016-02-24 13:21:10 +0530

    event handler added

--- image_share.js
+++ image_share.js
@@ -16,6 +16,13 @@ if (Meteor.isClient) {
   ];
 
   Template.images.helpers({images:img_data});
+
+  Template.images.events({
+    'click .js-image': function(event){
+      $(event.target).css("width", "50px");
+
+    }
+  });
 }
 
 if (Meteor.isServer) {
