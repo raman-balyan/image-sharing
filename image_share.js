f8091a376549278d64008309b8a67239524d6b5a
Raman <raman.balyan@gmail.com>
2016-02-26 10:31:08 +0530

    uploaded images connected to user

--- image_share.js
+++ image_share.js
@@ -5,7 +5,18 @@ if (Meteor.isClient) {
     passwordSignupFields: "USERNAME_AND_EMAIL"
   });
 
-  Template.images.helpers({images:Images.find({}, {sort:{createdOn:-1, rating:-1}})});
+  Template.images.helpers({
+  images:Images.find({}, {sort:{createdOn:-1, rating:-1}}),
+  getUser: function(user_id){
+    var user = Meteor.users.findOne({_id:user_id});
+    if (user){
+      return user.username;
+    }
+    else{
+      return "anon";
+    }
+  }
+  });
 
   Template.body.helpers({username: function(){
     if(Meteor.user()){
@@ -45,12 +56,14 @@ if (Meteor.isClient) {
     'submit .js-add-image': function(event){
       var img_src = event.target.img_src.value;
       var img_alt = event.target.img_alt.value;
-
-      Images.insert({
-        img_src: img_src,
-        img_alt: img_alt,
-        createdOn: new Date()
+      if(Meteor.user()){
+        Images.insert({
+          img_src: img_src,
+          img_alt: img_alt,
+          createdOn: new Date(),
+          createdBy: Meteor.user()._id
       });
+      }
       $("#image_add_form").modal('hide');
       return false;
     }
