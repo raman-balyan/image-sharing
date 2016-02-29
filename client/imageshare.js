0162b7c7b2cef441dce69b719e3f6ee8c202610c
Raman <raman.balyan@gmail.com>
2016-02-29 12:34:26 +0530

    routing is done

--- client/imageshare.js
+++ client/imageshare.js
@@ -1,3 +1,39 @@
+// routing
+
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function(){
+  this.render('welcome',{
+    to: "main"
+  }); 
+
+});
+
+Router.route('/images', function(){
+  this.render('navbar',{
+    to: "navbar"
+  });
+  this.render('images',{
+    to: "main"
+  }); 
+
+});
+
+Router.route('/image/:_id', function(){
+  this.render('navbar',{
+    to: "navbar"
+  });
+  this.render('image',{
+    to: "main",
+    data: function(){
+      return Images.findOne({_id:this.params._id});
+    }
+  }); 
+
+});
+
   Session.set("imageLimit", 6);
   lastScrollTop = 0;
   $(window).scroll(function(event){
